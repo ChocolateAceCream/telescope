a13ea005bd737aba88d90911eca91a0aecbf41eb
ChocolateAceCream <nuodi@hotmail.com>
2025-01-27 19:48:02 -0800

    FEAT: add login page

--- frontend/src/views/auth/login.tsx
+++ frontend/src/views/auth/login.tsx
@@ -1,56 +1,31 @@
-// src/components/LoginPage.jsx
-
-import { TextField, Box, Typography, Container, Button } from '@mui/material'
-import MyButton from '@/components/button'
+import React from 'react'
+import {
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  Checkbox,
+  FormControlLabel,
+} from '@mui/material'
+// import { FcGoogle } from 'react-icons/fc'
 import MyForm from '@/components/form'
 import { useState } from 'react'
-import { postLogin } from '@/api/auth'
-import {
-  postUpload,
-  getUploadPresignedUrl,
-  putS3Upload,
-  getDownloadPresignedUrl,
-} from '@/api/service'
+import MyButton from '@/components/button'
 import { sha256 } from '@/utils/encryption'
-import { styled } from '@mui/material/styles'
+import { postLogin } from '@/api/auth'
 
 interface FormData {
   username: string
   password: string
 }
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
-})
-
 const LoginPage = () => {
-  const [backgroundImageUrl, setBackgroundImageUrl] = useState<string | null>(
-    null
-  )
   const [formData, setFormData] = useState<FormData>({
     username: '',
     password: '',
   } as FormData)
 
-  const handleSSE = async () => {
-    const es = new EventSource('/backend/api/v1/sse/subscribe')
-    // Whenever the connection is established between the server and the client we'll get notified
-    es.onopen = (e) => console.log('>>> Connection opened!', e)
-    // Made a mistake, or something bad happened on the server? We get notified here
-    es.onerror = (e) => console.log('ERROR!', e)
-    // This is where we get the messages. The event is an object and we're interested in its `data` property
-    es.onmessage = (e) => {
-      console.log('>>>', e.data)
-    }
-  }
   const handleSubmit = async (event: React.FormEvent<HTMLDivElement>) => {
     console.log('Form data submitted:', formData, sha256(formData.password))
     const { data: res } = await postLogin({
@@ -59,93 +34,71 @@ const LoginPage = () => {
     })
     console.log('Login result:', res)
   }
-  const handleUpload = async () => {
-    const { data: res2 } = await postUpload()
-    console.log('upload result:', res2)
-  }
-
-  const handleDownload = async () => {
-    const { data: res } = await getDownloadPresignedUrl('logo.png')
-    console.log('handleDownload result:', res)
-    setBackgroundImageUrl(res.data.url)
-  }
-
-  const handleS3Upload = async (e: any) => {
-    console.log(e.target.files[0].name)
-    const payload = {
-      file_name: e.target.files[0].name,
-    }
-    const { data: res } = await getUploadPresignedUrl(payload)
-    console.log('get url result:', res)
-
-    const { data: res2 } = await putS3Upload(
-      res.data.presigned_url,
-      e.target.files[0]
-    )
-    console.log('upload result:', res2)
-  }
   return (
-    <Box
-      maxWidth="sm"
-      className="p-8 bg-lite-orange shadow-lg rounded-lg m-auto "
-      sx={{
-        backgroundImage: backgroundImageUrl
-          ? `url(${backgroundImageUrl})`
-          : 'none',
-      }}
-    >
-      <Button onClick={handleSSE}>start sse</Button>
-      <Button onClick={handleUpload}>upload</Button>
-      <Button onClick={handleDownload}>download</Button>
-      <Button
-        component="label"
-        role={undefined}
-        variant="contained"
-        tabIndex={-1}
-      >
-        Upload files
-        <VisuallyHiddenInput type="file" onChange={handleS3Upload} multiple />
-      </Button>
-      <Typography variant="h4" className="mb-4 font-bold text-gray-800">
-        Login
-      </Typography>
-      <MyForm
-        formData={formData}
-        setFormData={setFormData}
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="username"
-          label="Username"
-          name="username"
-          autoFocus
-          className="mb-4"
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          className="mb-6"
-        />
-        <MyButton
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className="py-2"
-        >
-          Sign In
-        </MyButton>
-      </MyForm>
-    </Box>
+    <div className="flex items-center justify-center min-h-screen">
+      <Card className="max-w-sm p-8 shadow-lg rounded-3xl">
+        <CardContent>
+          <Typography
+            variant="h4"
+            component="h2"
+            className="mb-2 text-center font-bold text-gray-800"
+          >
+            WELCOME BACK
+          </Typography>
+          <Typography
+            variant="body1"
+            className="mb-6 text-center text-gray-500"
+          >
+            Welcome back! Please enter your details.
+          </Typography>
+          <MyForm
+            formData={formData}
+            setFormData={setFormData}
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoFocus
+              className="mb-4"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              className="mb-6"
+            />
+            <MyButton
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className="py-2"
+            >
+              Sign In
+            </MyButton>
+          </MyForm>
+          <Typography
+            variant="body2"
+            className="mt-6 text-center text-gray-600"
+          >
+            Don&#39;t have an account?{' '}
+            <a href="#" className="text-red-500 hover:underline">
+              Sign up for free!
+            </a>
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
